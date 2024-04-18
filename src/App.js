2e0997aa3748f26661b8716821e5f73c94e4f188
zarashah14 <zarashahhussain22@gmail.com>
2024-04-18 20:16:39 +0500

    updatedz

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import './App.css';
-// import Bgchanger from './Components/BGChanger/Bg';
+
 import Home from './Components/Home/Home';
 // import Password from './Components/PasswordGenerator/Password';
+// import Bgchanger from './Components/BGChanger/Bg';
 function App() {
   return (
     <div className="App">
-     {/* <div className='text-gray-900 text-center mt-4 font-bold text-3xl'>bg changer</div> */}
+     
      {/* <Bgchanger/> */}
-     <Home/>
      {/* <Password /> */}
+     <Home/>
     </div>
   );
 }
